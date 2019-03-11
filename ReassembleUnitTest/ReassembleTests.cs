60553ce25052086693961c16225583d8ce9b39fe
Ujval Shah <shah.ujval@gmail.com>
2019-03-11 17:26:37 +0800

    Add files via upload

--- ReassembleUnitTest/ReassembleTests.cs
+++ ReassembleUnitTest/ReassembleTests.cs
@@ -68,6 +68,71 @@ namespace ReassembleUnitTest
             reassemble.Dispose();
         }
 
+        /// <summary>
+        /// Test With 2 fragments with no overlap
+        /// </summary>
+        [TestMethod]
+        public void TestReassembleWithDataSetExample3()
+        {
+
+            List<string> stringFragmentsTest = new List<string>(new string[] { "Go Bucks", "Beat Michigan"});  // Go Bucks Beat Michigan
+
+            string expectedFinalMergedString = "Go BucksBeat Michigan";
+
+            // Step 1 - intializes the String Fragments
+            //Create and object of Reassemble (this intializes the String Fragments)
+            Reassemble reassemble = new Reassemble();
+            reassemble.StringFragments = stringFragmentsTest;
+
+            // Continue until you have only one item in collection
+            while (reassemble.StringFragments.Count != 1)
+            {
+                //Step 2. Find the Longest Overlap in string fragments
+                reassemble.FindLongestOverlap();
+
+                //Step 3. Merge the found frangments with largest overlap and Contiue the process.    
+                reassemble.MergeTheOverLapFragments();
+
+                // - Refresh the Counter for Next round
+                reassemble.RefreshCountersForNextRound();
+            }
+
+            Assert.AreEqual(expectedFinalMergedString, reassemble.StringFragments[0]);
+            reassemble.Dispose();
+        }
+        /// <summary>
+        /// Test With More than 3 fragments with no overlap
+        /// </summary>
+        [TestMethod]
+        public void TestReassembleWithDataSetExample4()
+        {
+
+            List<string> stringFragmentsTest = new List<string>(new string[] { "Go Bucks", "Beat Michigan" , " alaska" , " miami"});  // Go Bucks Beat Michigan
+
+            string expectedFinalMergedString = "Go BucksBeat Michigan alaska miami";
+
+            // Step 1 - intializes the String Fragments
+            //Create and object of Reassemble (this intializes the String Fragments)
+            Reassemble reassemble = new Reassemble();
+            reassemble.StringFragments = stringFragmentsTest;
+
+            // Continue until you have only one item in collection
+            while (reassemble.StringFragments.Count != 1)
+            {
+                //Step 2. Find the Longest Overlap in string fragments
+                reassemble.FindLongestOverlap();
+
+                //Step 3. Merge the found frangments with largest overlap and Contiue the process.    
+                reassemble.MergeTheOverLapFragments();
+
+                // - Refresh the Counter for Next round
+                reassemble.RefreshCountersForNextRound();
+            }
+
+            Assert.AreEqual(expectedFinalMergedString, reassemble.StringFragments[0]);
+            reassemble.Dispose();
+        }
+
         [TestMethod]
         public void NullStringTest()
         {
