55ea12b7ec3eae4f21b6bc34455973343ffe6116
Ujval Shah <shah.ujval@gmail.com>
2019-03-11 16:05:44 +0800

    Add files via upload

---
+++ ReassembleUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReassembleUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReassembleUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc77117b-5f2a-4688-8af0-4ffdd100c98d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReassembleUnitTest/ReassembleTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringOverlap;
+
+namespace ReassembleUnitTest
+{
+    [TestClass]
+    public class ReassembleTests
+    {
+        [TestMethod]
+        public void TestReassembleWithDataSetExample1()
+        {
+
+            List<string> stringFragmentsTest = new List<string>(new string[] { "all is well", "ell that en", "hat end", "t ends well" }); // all is well that ends well
+
+            string expectedFinalMergedString = "all is well that ends well";
+
+            // Step 1 - intializes the String Fragments
+            //Create and object of Reassemble (this intializes the String Fragments)
+            Reassemble reassemble = new Reassemble();
+            reassemble.StringFragments = stringFragmentsTest;
+
+            // Continue until you have only one item in collection
+            while (reassemble.StringFragments.Count != 1)
+            {
+                //Step 2. Find the Longest Overlap in string fragments
+                reassemble.FindLongestOverlap();
+
+                //Step 3. Merge the found frangments with largest overlap and Contiue the process.    
+                reassemble.MergeTheOverLapFragments();
+
+                // - Refresh the Counter for Next round
+                reassemble.RefreshCountersForNextRound();
+            }
+
+            Assert.AreEqual(expectedFinalMergedString, reassemble.StringFragments[0]);
+            reassemble.Dispose();
+        }
+
+        [TestMethod]
+        public void TestReassembleWithDataSetExample2()
+        {
+
+            List<string> stringFragmentsTest = new List<string>(new string[] { "Bucks -- Beat", "Go Bucks", "o Bucks -- B", "Beat Mich", "ichigan~", "Bucks", "Michigan~" });  // Go Bucks -- Beat Michigan~
+
+            string expectedFinalMergedString = "Go Bucks -- Beat Michigan~";
+
+            // Step 1 - intializes the String Fragments
+            //Create and object of Reassemble (this intializes the String Fragments)
+            Reassemble reassemble = new Reassemble();
+            reassemble.StringFragments = stringFragmentsTest;
+
+            // Continue until you have only one item in collection
+            while (reassemble.StringFragments.Count != 1)
+            {
+                //Step 2. Find the Longest Overlap in string fragments
+                reassemble.FindLongestOverlap();
+
+                //Step 3. Merge the found frangments with largest overlap and Contiue the process.    
+                reassemble.MergeTheOverLapFragments();
+
+                // - Refresh the Counter for Next round
+                reassemble.RefreshCountersForNextRound();
+            }
+
+            Assert.AreEqual(expectedFinalMergedString, reassemble.StringFragments[0]);
+            reassemble.Dispose();
+        }
+
+        [TestMethod]
+        public void NullStringTest()
+        {
+            // Step 1 - intializes the String Fragments
+            //Create and object of Reassemble (this intializes the String Fragments)
+            Reassemble reassemble = new Reassemble();
+            reassemble.StringFragments = null;
+            // Only one or no string fragment present - Return as it is.
+            if (!reassemble.isValidMinimumCount())
+            {
+                Assert.AreEqual(null, reassemble.StringFragments);
+            }
+            reassemble.Dispose();
+        }
+    }
+}
